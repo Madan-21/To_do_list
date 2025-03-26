2ab652d23832778b669e2ebc7ec084186fcd55a6
Madan Pandey <pmadan466@gmail.com>
2025-03-26 12:46:41 +0545

    Rename todo.html to index.html

--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,38 @@
-const CACHE_NAME = 'todo-cache-v1';
+const CACHE_NAME = "todo-cache-v1";
 const urlsToCache = [
-    './',
-    './index.html',
-    './manifest.json',
-    './style.css', // Add your CSS file path
-    './script.js', // Add your JS file path
-    './icons/icon-192x192.png',
-    './icons/icon-512x512.png'
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./style.css", // Add your CSS file path
+  "./script.js", // Add your JS file path
+  "./icons/icon-192x192.png",
+  "./icons/icon-512x512.png",
 ];
 
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-    );
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => response || fetch(event.request))
-    );
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
 });
 
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames =>
-            Promise.all(
-                cacheNames.map(cache => {
-                    if (cache !== CACHE_NAME) {
-                        return caches.delete(cache);
-                    }
-                })
-            )
-        )
-    );
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      )
+    )
+  );
 });
